faa1e34c1084f1487c808039901fc17364b30391
Yi Tang <toyeduck@Yi.local>
2016-12-06 16:51:20 -0500

    Add Model to admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+from .models import Choice
+from .models import Bet
+
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Bet)
