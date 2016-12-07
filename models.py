6f7a21aa716040b09f8b158db64f231edb282db0
Yi Tang <toyeduck@Yi.local>
2016-12-07 02:51:35 -0500

    Final Draftgit status

--- models.py
+++ models.py
@@ -11,6 +11,8 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    total_bet = models.IntegerField(default=0)
+    chance_to_win = models.IntegerField(default=0)
     #votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
--- views.py
+++ views.py
@@ -5,8 +5,34 @@ from .models import Question, Choice, Bet
 from django.shortcuts import get_object_or_404, render
 from django import forms
 from django.template import loader
+from django.utils import timezone
 
 def index(request):
+	#Update total bets
+	question_list = Question.objects.all()
+	for question in question_list:
+		choice_list = question.choice_set.all()
+		for choice in choice_list:
+			total_choice_bet = 0
+			bet_list = choice.bet_set.all()
+			for bet in bet_list:
+				total_choice_bet += bet.bet_amount
+			choice.total_bet = total_choice_bet
+			choice.save()
+
+	#Calculate chance to win
+	updated_question_list = Question.objects.all()
+	for question in updated_question_list:
+		choice_list = question.choice_set.all()
+		total_bet = 0.0
+		for choice in choice_list:
+			total_bet += choice.total_bet
+		#print(total_bet)
+		if total_bet != 0.0:
+			for choice in choice_list:
+				choice.chance_to_win = int(choice.total_bet*100/total_bet)
+				choice.save()
+
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
 	template = loader.get_template('infomarket/index.html')
@@ -23,7 +49,22 @@ def login(request):
 	return render(request, 'infomarket/login.html')
 
 def create(request):
-	return render(request, 'infomarket/create.html')
+	try:
+		new_question = Question(question_text=request.POST['question'], pub_date=timezone.now(), pub_user=request.POST['name'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question creating form.
+		return render(request, 'infomarket/create.html', {
+			#'question': question,
+			'error_message': "You didn't input a question.",
+		})
+	else:
+		new_question.save()
+		new_bet_1 = new_question.choice_set.create(choice_text=request.POST['outcome-a'],total_bet=0)
+		new_bet_1.save()
+		new_bet_2 = new_question.choice_set.create(choice_text=request.POST['outcome-b'],total_bet=0)
+		new_bet_2.save()
+		#return HttpResponseRedirect(reverse('index', args=(question.id,)))
+		return render(request, 'infomarket/create.html')
 
 # def place(request):
 # 	return render(request, 'infomarket/place.html')
@@ -42,7 +83,8 @@ def place(request, question_id):
 		selected_bet = selected_choice.bet_set.create(bet_user=request.POST['bet_user_name'],bet_amount=10)
 		#selected_choice.votes += 1
 		selected_bet.save()
-		return HttpResponseRedirect(reverse('index', args=(question.id,)))
+		return HttpResponseRedirect(reverse('place', args=(question.id,)))
+		#return HttpResponseRedirect(reverse('index', args=(question.id,)))
 		# Always return an HttpResponseRedirect after successfully dealing
 		# with POST data. This prevents data from being posted twice if a
 		# user hits the Back button.
